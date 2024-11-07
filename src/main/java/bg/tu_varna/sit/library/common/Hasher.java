d909d17ac1b4cb0ba3ef1bbf83797a1184a9fc39
Lachezar Jordanov <122541444+Laacho@users.noreply.github.com>
2024-11-07 15:38:53 +0200

    implemented Hasher algorithm (#17)

---
+++ src/main/java/bg/tu_varna/sit/library/common/Hasher.java
@@ -0,0 +1,29 @@
+package bg.tu_varna.sit.library.common;
+
+
+import org.apache.log4j.Logger;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Base64;
+
+public class Hasher {
+    private static final Logger log = Logger.getLogger(Hasher.class);
+
+    //https://www.geeksforgeeks.org/sha-512-hash-in-java/
+    public static String hashPassword(String password) {
+        try {
+            MessageDigest md = MessageDigest.getInstance("SHA-256");
+            byte[] hashedBytes = md.digest(password.getBytes());
+            return Base64.getEncoder().encodeToString(hashedBytes);
+        } catch (NoSuchAlgorithmException e) {
+            log.error("Password hashing failed: " + e.getMessage());
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static boolean verifyPassword(String enteredPassword, String storedHash) {
+        String hashedInputPassword = hashPassword(enteredPassword);
+        return hashedInputPassword.equals(storedHash);
+    }
+}
