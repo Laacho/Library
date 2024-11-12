9a18e9876e9e8fd7ee906b04fa9a63ff61fec31a
Diyan Dimitrov <122541336+didooooo@users.noreply.github.com>
2024-11-12 13:22:30 +0200

    started implementing processors (#23)

---
+++ src/main/java/bg/tu_varna/sit/library/core/register/BaseProcessor.java
@@ -0,0 +1,9 @@
+package bg.tu_varna.sit.library.core.register;
+
+import bg.tu_varna.sit.library.common.converters.common.ConversionService;
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+public class BaseProcessor {
+    protected final ConversionService conversionService;
+}
